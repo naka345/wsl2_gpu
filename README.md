0d7106e3f8ef460d79a9b430b05f4ad4c2aacab5
naka345 <nakaka33@yahoo.co.jp>
2020-07-02 22:07:55 +0900

    add comment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wsl2_gpu
-gpu対応
+[wsl2]()でCUDAを用いてGPUを使えるようにした。
+
+[nvidiaのフォーラム](https://forums.developer.nvidia.com/t/hiccups-setting-up-wsl2-cuda/128641)で言われているように、
+現段階では `docker desktop WSL 2`経由だとGPUサポートされていないため`nvidia-container-toolkit`か`nvidia-docker2`経由で使う必要がある。
+予め`docker desktop WSL 2`を使用している環境だとwin上の `docker desktop` を見に行こうとするため、設定で
--- script/nvidia-docker.sh
+++ script/nvidia-docker.sh
@@ -1,3 +1,4 @@
+# install cuda (onry cuda-toolkit)
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
@@ -9,12 +10,14 @@ echo "set -x PATH /usr/local/cuda-11.0/bin \$PATH" >> .config/fish/config.fish
 echo "set -x LD_LIBRARY_PATH /usr/local/cuda-11.0/lib64 \$LD_LIBRARY_PATH" >> .config/fish/config.fish
 source .config/fish/config.fish
 
+# recognized GPU
 cd /usr/local/cuda/samples/4_Finance/BlackScholes
 sudo make
 ./BlackScholes
 
 cd ~
 
+# install nvidia-docker2
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
@@ -23,4 +26,5 @@ curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distributi
 sudo apt-get update && sudo apt-get install -y nvidia-docker2
 sudo service docker restart
 
-docker run --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody nbody -gpu -benchmark 
+# runnung cuda benchmark
+docker run --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody nbody -gpu -benchmark
