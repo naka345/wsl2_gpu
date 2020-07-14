9ff3cd0bdf71cab1b165d4a50ee84c9636fcbb37
naka345 <nakaka33@gmail.com>
2020-07-14 14:44:28 +0900

    change package

--- script/nvidia-docker.sh
+++ script/nvidia-docker.sh
@@ -27,7 +27,7 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container-experimental.list | sudo tee /etc/apt/sources.list.d/libnvidia-container-experimental.list
 
-sudo apt-get update && sudo apt-get install -y nvidia-docker2
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 sudo service docker restart
 
 # runnung cuda benchmark
